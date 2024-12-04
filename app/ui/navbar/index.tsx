2ab2167b5a332bec61be79d93857852b57672093
Behzad Haji Ahmadi <bh.ahmady@gmail.com>
2024-12-04 15:26:46 +0330

    test ws

--- app/ui/navbar/index.tsx
+++ app/ui/navbar/index.tsx
@@ -1,28 +1,11 @@
 export default function MainNavbar() {
   return (
-    <>
-      <div className='navbar bg-base-200'>
-        <div className='navbar-start'>
-          <div className='dropdown'>
-            <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                className='inline-block h-5 w-5 stroke-current'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h16M4 18h16'
-                ></path>
-              </svg>
-            </div>
-          </div>
-          <a className='btn btn-ghost text-xl'>Crypto APP</a>
-        </div>
+    <nav>
+      <div className='flex gap-1'>
+        <span>Modern Idea</span>
+        <span>|</span>
+        <span>Crypto Watch</span>
       </div>
-    </>
+    </nav>
   )
 }
